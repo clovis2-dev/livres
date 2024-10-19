167f5cc728d95ea52f79f8ed2a1b78de64b30787
fanmoe pouemo clovis <clovis.fanmoepouemo@monboreal.ca>
2024-10-19 11:14:13 -0800

    ajout de la classe revue et creation d<instances

--- AppConsoleLivres/Livre.cs
+++ AppConsoleLivres/Livre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppConsoleLivres;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace AppConsoleLivres
 {
     internal class Livre
     {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        puvlic int NombrePages { get; set; }
+        public Livre {string titre,string Auteur,int NombrePages}
+       {Titre = titre;
+        Auteur = auteur;
+        NombrePages = nbrpage;
+        }
+      public virtual voie AfficherDettail()
+            {
+    Console.WriteLine($" Livre: {Titre}  auteur: {Auteur} nombre de page :{NombrePage}");
+             }
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue
+    {
+        public string Titre { get; set; }
+        public string Domaine { get; set; }
+        public DateTime DatePublication{get; set;}
+    }
+    public Revue(string titre, string domaine, DateTime datePublication)
+    {
+        Titre = titre;
+        Domaine = domaine;
+        DatePublication = datePublication;
+    }
+    public void AfficherDetails()
+    {
+        Console.WriteLine($"Titre: {Titre}, Domaine: {Domaine}, Date de publication: {DatePublication}");
+    }
+}
