f6e6aa63275d277ca42489e1a6e8aedbfa6d096d
clovis2-dev <clovis.fanmoepouemo@monboreal.ca>
2024-11-17 11:02:48 -0500

    Add files via upload

---
+++ Roman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public DateTime DatePublication { get; set; }
+        public Roman(string titre, string auteur, DateTime datePublication)
+
+        {
+            Titre = titre;
+            Auteur = auteur;
+            DatePublication = datePublication;
+        }
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Date de publication: {DatePublication.ToShortDateString()}");
+        }
+    }
+}
