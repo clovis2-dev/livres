359bf91c70f10d3aee297e3219920fbee2fcffcb
clovis2-dev <clovis.fanmoepouemo@monboreal.ca>
2024-11-17 11:02:07 -0500

    Add files via upload

---
+++ Livre.cs
@@ -0,0 +1,25 @@
+﻿using AppConsoleLivres;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        puvlic int NombrePages { get; set; }
+        public Livre {string titre,string Auteur,int NombrePages}
+       {Titre = titre;
+        Auteur = auteur;
+        NombrePages = nbrpage;
+        }
+      public virtual voie AfficherDettail()
+            {
+    Console.WriteLine($" Livre: {Titre}  auteur: {Auteur} nombre de page :{NombrePage}");
+             }
+    }
+}
